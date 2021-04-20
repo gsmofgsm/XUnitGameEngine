a410e5e65d0d0ee1e5b2f9fc74ff0f3f9eb44e6a
Qing Ma <q.ma@ncontrol.cloud>
2021-04-20 20:00:36 +0200

    Assert on Floating Point Value

---
+++ GameEngine.Tests/BossEnemyShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+            Assert.Equal(166.667, sut.SpecialAttackPower, 3);
+        }
+    }
+}
