7c747c7210222967caf2be8905f62384ccc0e27b
Qing Ma <q.ma@ncontrol.cloud>
2021-04-20 20:01:50 +0200

    Assert Null Values

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -77,5 +77,13 @@ namespace GameEngine.Tests
             //Assert.True(sut.Health >= 101 && sut.Health <= 200);
             Assert.InRange<int>(sut.Health, 101, 200);
         }
+
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Null(sut.Nickname);
+        }
     }
 }
