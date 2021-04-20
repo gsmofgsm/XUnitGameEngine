38c2e73f4e0f3f0f27700f217efd7e474cddd237
Qing Ma <q.ma@ncontrol.cloud>
2021-04-20 13:51:37 +0200

    String Value Assertions

--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -12,5 +12,51 @@ namespace GameEngine.Tests
 
             Assert.True(sut.IsNoob);
         }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Equal("Sarah Smith", sut.FullName);
+            Assert.StartsWith("Sarah", sut.FullName);
+            Assert.EndsWith("Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "SARAH";
+            sut.LastName = "SMITH";
+
+            Assert.Equal("Sarah Smith", sut.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void CalculateFullName_SubstringAssert()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Contains("ah Sm", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_RegExpAssert()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+        }
     }
 }
