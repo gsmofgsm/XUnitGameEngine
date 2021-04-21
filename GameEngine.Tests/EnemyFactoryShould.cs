db9d48a103fbe2e88aea82852c6c748fea4daca6
Qing Ma <q.ma@ncontrol.cloud>
2021-04-21 12:36:15 +0200

    Assert on Exceptions

--- GameEngine.Tests/EnemyFactoryShould.cs
+++ GameEngine.Tests/EnemyFactoryShould.cs
@@ -75,5 +75,25 @@ namespace GameEngine.Tests
             //Assert.Same(enemy1, enemy2); // fail
             Assert.NotSame(enemy1, enemy2);
         }
+
+        [Fact]
+        public void NotAllowNullName()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            //Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+            Assert.Throws<ArgumentNullException>("name", () => sut.Create(null)); // argument name
+        }
+
+        [Fact]
+        public void OnlyAllowKingOrQueenBossEnemies()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            EnemyCreationException ex =
+                Assert.Throws<EnemyCreationException>(() => sut.Create("Zombie", true));
+
+            Assert.Equal("Zombie", ex.RequestedEnemyName);
+        }
     }
 }
