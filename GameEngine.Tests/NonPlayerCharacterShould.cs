0003679ae64d4750ace4fa13e5a26ede53aa9a39
Qing Ma <q.ma@ncontrol.cloud>
2021-04-23 22:40:34 +0200

    Share test data across tests using MemberData

--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -10,10 +10,8 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {
         [Theory]
-        [InlineData(0, 100)]
-        [InlineData(1, 99)]
-        [InlineData(50, 50)]
-        [InlineData(101, 1)]
+        [MemberData(nameof(InternalHealthDamageTestData.TestData), 
+            MemberType = typeof(InternalHealthDamageTestData))]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
--- GameEngine.Tests/PlayerCharacterShould.cs
+++ GameEngine.Tests/PlayerCharacterShould.cs
@@ -145,10 +145,8 @@ namespace GameEngine.Tests
         }
 
         [Theory]
-        [InlineData(0, 100)]
-        [InlineData(1, 99)]
-        [InlineData(50, 50)]
-        [InlineData(101, 1)]
+        [MemberData(nameof(InternalHealthDamageTestData.TestData),
+            MemberType = typeof(InternalHealthDamageTestData))]
         public void TakeDamage(int damage, int expectedHealth)
         {
             _sut.TakeDamage(damage);
