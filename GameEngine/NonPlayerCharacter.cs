a0be8484786d7e4494e483622b5ae8220a87bfb2
Qing Ma <q.ma@ncontrol.cloud>
2021-04-23 22:24:06 +0200

    NonPlayerCharacterShould TakeDamage just as PlayerCharacterShould

---
+++ GameEngine/NonPlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    public class NonPlayerCharacter
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public int Health { get; set; } = 100;
+
+        public void TakeDamage(int damage)
+        {
+            Health = Math.Max(1, Health -= damage);
+        }
+    }
+}
